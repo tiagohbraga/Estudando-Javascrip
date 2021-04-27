be0132c58f9a1da8f5def7ea4e95704a8859baf7
tiagohbraga <tiagohbraga@gmail.com>
2021-04-27 11:36:05 -0300

    aula-4
    aprendendo sobre string (cadeia de caracteres)

--- comentarios.js
+++ comentarios.js
@@ -1,3 +1,13 @@
 // para usar comentario em linha no javascrip eu uso essas duas barras no começo
 /* para criar um comentario em bloco eu inicio com os caracteres que estão no inicio dessa linha e fecho com os que estão no final */
-console.log('Bem vindos ao Starter!!')
+//console.log('Bem vindos ao Starter!!')
+
+/*
+ String
+
+    * Cadeia de caracteres
+
+    "" - aspas duplas
+    '' - aspas siples
+    `` - templete literals ou tamplate strings (posso inclusive fazer multilinhas)
+*/
