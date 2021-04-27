1e92d9277d7d312e7cbe0eab97f6a3c52639c4ff
tiagohbraga <tiagohbraga@gmail.com>
2021-04-27 11:04:47 -0300

    aula-2
    aprendendo sobre comentarios no js, criei uma copia do .js só para anotar os comentarios das aulas

---
+++ comentarios.js
@@ -0,0 +1,3 @@
+// para usar comentario em linha no javascrip eu uso essas duas barras no começo
+/* para criar um comentario em bloco eu inicio com os caracteres que estão no inicio dessa linha e fecho com os que estão no final */
+console.log('Bem vindos ao Starter!!')
