5e03ded578c6f3817d54d9a839723ad1c81df52e
tiagohbraga <tiagohbraga@gmail.com>
2021-05-06 15:45:24 -0300

    Operator precedence

--- comentarios.js
+++ comentarios.js
@@ -528,4 +528,51 @@ concatenation (concatenação)
     console.log(alpha + "s" + 15)
     */
 
+/*
+Type conversion (typecasting) vs Type coersion
+
+console.log (Number ('9') + 5)
+
+FALSY
+Quado um valor é considerado false em contextos onde um Booleano é obrigatorio (condicionais e loops)
+false
+0
+-0
+""
+null
+undefined
+NaN
+console.log( 0 ? 'verdadeiro' : 'falso')
+
+
+TRUTHY
+Quando um valor é considerado true em contexto onde um Booleano é obrigatório (condicionais e loops)
+true
+{}
+[]
+1
+3.23
+"0"
+"false"
+-1
+Infinity
+-Infinity
+console.log( [] ? 'verdadeiro' : 'falso')
+*/
+
+/*Operator precedence (precedência de operadores)
+
+    grouping - tem mais valor           ()
+    negação e incremento                ! ++ --
+    multiplicação e divisão             * /
+    adição e subtração                  +-
+    ralacional                          < <= > >=c
+    igualdade                           == != === !==
+    AND                                 &&
+    OR                                  ||
+    condicional                         ?:
+    assignment (atribuição)             = += -= *=
+*/
+
+
 
