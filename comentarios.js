653a1907f13a7dead83957f5888875720d45fbe3
tiagohbraga <tiagohbraga@gmail.com>
2021-05-09 11:19:31 -0300

    Celsius em Fahrenheit
    exercício

--- comentarios.js
+++ comentarios.js
@@ -764,3 +764,43 @@ function calculateBalance() {
 }
 calculateBalance()
 */
+
+/*Celsius em Fahrenheit
+    crie uma função que receba uma string em celsius ou fahrenhait e faça a transformação de uma unidade para a outra 
+    
+    C = (F - 32) * 5/9
+
+    F= C * 9/5 + 32
+        
+    //tansfomDegree('50F')
+function transformeDegree (degree){
+    const celsiusExist = degree.toUpperCase().includes ('C')
+    const fahrenhaitExist = degree.toUpperCase().includes('F')
+
+    //Fluxo de erro
+    if(!celsiusExist && !fahrenhaitExist){
+        throw new Error ('Grau não identificado')
+    }
+    //fluxo ideal, F -> C
+        let updatedDegree = Number(degree.toUpperCase().replace("F", ""));
+        let formula = fahrenhait => (fahrenhait - 32) * 5/9
+        let degreeSign = '°C'
+
+    //fluxo alternativo C -> F
+    if (celsiusExist){
+         updatedDegree = Number(degree.toUpperCase().replace("C", ""));
+         formula = ceusius => ceusius * 9/5 + 32
+         degreeSign = '°F'
+    }
+
+    return formula (updatedDegree) + degreeSign
+
+}
+try {
+    console.log(transformeDegree ('10C'))
+    console.log(transformeDegree ('50F'))
+    transformeDegree ('50Z')
+}catch (error) {
+    console.log(error.message)
+}
+*/
